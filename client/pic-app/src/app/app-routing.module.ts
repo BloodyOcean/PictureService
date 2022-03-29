951eef5c3f26e4d83dff486493eff394a1543230
Eduard Kovalenko <cchugaister@gmail.com>
2022-03-29 20:58:09 +0300

    Add routing and components, add navbar

--- client/pic-app/src/app/app-routing.module.ts
+++ client/pic-app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddImageComponent } from './add-image/add-image.component';
+import { HomeComponent } from './home/home.component';
+import { ShowImageComponent } from './show-image/show-image.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', 
+    component:HomeComponent,
+    children: [
+      {path:'add', component:AddImageComponent},
+      {path:'images', component:ShowImageComponent}
+    ], 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/pic-app/src/app/app.module.ts
+++ client/pic-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddImageComponent } from './add-image/add-image.component';
+import { ShowImageComponent } from './show-image/show-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddImageComponent,
+    ShowImageComponent
   ],
   imports: [
     BrowserModule,
